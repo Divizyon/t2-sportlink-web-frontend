0ef35582e36192e33ea8f47b5609efcb9e52ad56
atesbey-design <tuncerbostancibasi@gmail.com>
2025-03-15 20:37:29 +0300

    Hotfix:improve import name

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import { StoreExample } from './components/StoreExample'
+import { StoreDemo } from './components/StoreDemo'
 
 export default function Home() {
   return (
     <main className="min-h-screen p-8">
       <h1 className="text-2xl font-bold mb-8">Zustand State Management Example</h1>
-      <StoreExample />
+      <StoreDemo />
     </main>
   )
 }
